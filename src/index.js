5bcb7452b41452d45fbe02bdf0462f5d27079393
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-12-25 11:19:36 +0200

    change basename to build

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/oksana-openko-site">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
